7d31cbdda8eb190779595bb7c7fb22063850252b
abdellahzou <132308879+abdellahzou@users.noreply.github.com>
2026-01-11 11:24:17 +0100

    Update Preview.tsx

--- components/Preview.tsx
+++ components/Preview.tsx
@@ -906,7 +906,7 @@ export const Preview: React.FC<PreviewProps> = ({ t, className }) => {
       const contentHeight = containerRef.current.scrollHeight;
 
       // Mobile print prediction: 
-      const perceivedPrintHeight = isMobile ? contentHeight * 0.79 : contentHeight;
+      const perceivedPrintHeight = isMobile ? contentHeight * 0.8 : contentHeight;
 
       if (perceivedPrintHeight > TARGET_HEIGHT) {
         // Content too big: Shrink
